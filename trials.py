d326be972070049de3a4c06206df81dbc94d9aca
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-30 20:41:58 +0000

    Complete Compression Function

--- trials.py
+++ trials.py
@@ -2,44 +2,143 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    """Print each item in given list"""
+
+    for item in items:
+        print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    """Given a list of numbers, return a list of all even numbers"""
+    even_nums = []
 
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    
+    return even_nums
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    """Given a list, return all elements at odd numbered indices"""
+
+    result = []
+
+    i = 0
+    for item in items:
+        if i % 2 != 0:
+            result.append(item)
+        i += 1
+    
+    return result
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    """Given a list, output a numbered list"""
+
+    i = 1
+
+    for item in items:
+        print(f'{i}. {item}')
+        i += 1
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    """Returns a list of numbers within a specific range"""
+
+    nums = []
+
+    num = start
+
+    while num < stop:
+        nums.append(num)
+        num += 1
 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    """Given a string, return a string where vowels are replaced with *"""
 
+    chars = []
+
+    for letter in word:
+        if letter in 'aeiou':
+            chars.append("*")
+        
+        else:
+            chars.append(letter)
+    
+    return ''.join(chars)
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
+    """Given a string in snake_case, return to upperCamelCase"""
+
+    camelCase = []
+
+    for item in string.split('_'):
+        camelCase.append(f'{item.title()}')
+    
+    return ''.join(camelCase)
 
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    """Return the length of the longest word in given list"""
 
+    longest = len(words[0])
+
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+
+    return longest
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    """Truncate repeating characetrs into one character"""
+
+    result = []
+
+    for char in string:
+        if len(result) == 0 or char != result[len(result) -1]:
+            result.append(char)
+
+    return ''.join(result)
 
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    """Return True if all parentheses in a given string are balanced"""
+
+    parens = 0;
+
+    for char in string:
+        if char == '(' or char == ')':
+            parens += 1
+        
+    if parens % 2 != 0:
+        return False
+    
+    return True
 
 
 def compress(string):
-    pass  # TODO: replace this line with your code
+    """Return compressed version of given string"""
+
+    compressed = []
+
+    curr_char = ''
+    char_count = 0
+
+    for char in string:
+        if char != curr_char:
+            compressed.append(curr_char)
+
+            if char_count > 1:
+                compressed.append(str(char_count))
+        
+            curr_char = char
+            char_count = 0
+        
+        char_count += 1
+    
+    compressed.append(curr_char)
+    if char_count > 1:
+        compressed.append(str(char_count))
+
+    return ''.join(compressed)
